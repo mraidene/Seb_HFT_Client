2c6480f184e6e07d459b803d9b569f0bf3e576e8
kebo1234 <kebo@uchicago.edu>
2025-05-23 17:05:39 -0500

    Update main_engine.cpp

--- main_engine.cpp
+++ main_engine.cpp
@@ -1,8 +1,11 @@
+// main_engine.cpp
 #include <boost/asio.hpp>
 #include <iostream>
 #include <cstring>
 #include <chrono>
 #include <unordered_map>
+#include <thread>                     // for std::this_thread::yield()
+#include <boost/system/error_code.hpp>
 #include "add_challenge_id.h"
 #include "add_ticker.h"
 #include "add_bid.h"
@@ -11,123 +14,132 @@
 #include "initialize_udp_connection.h"
 #include "initialize_tcp_connection.h"
 #include "handle_line.h"
+
 using boost::asio::ip::udp;
 using namespace std;
 using namespace chrono;
 
 int trade(int slots, int char_per_slot, std::string name) {
     try {
-        // Initializing io_context
         boost::asio::io_context io_context;
 
-        // Initializing UDP Connection
-        const std::string udp_ip = "239.255.0.1";
-        const int udp_port = 3001;
+        // UDP setup
+        const std::string udp_ip   = "239.255.0.1";
+        const int         udp_port = 3001;
         udp::socket udp_socket = initialize_udp_connection(io_context, udp_ip, udp_port);
 
-        // Initializing TCP Connection ("127.0.0.1" for local testing)
-        const std::string tcp_ip = "127.0.0.1";
-        const int tcp_port = 4000;
-        tcp::socket tcp_socket = initialize_tcp_connection(io_context, tcp_ip, tcp_port);
+        // TCP setup
+        const std::string tcp_ip   = "127.0.0.1";
+        const int         tcp_port = 4000;
+        auto tcp_socket = initialize_tcp_connection(io_context, tcp_ip, tcp_port);
 
-        // Converting Trader Name to Char*
+        // Trader name
         char trader_name[name.length() + 1];
-        const size_t trader_name_length = sizeof(trader_name);
+        const size_t trader_name_length = name.length();
         strcpy(trader_name, name.c_str());
 
-        // Initialize Buffer and Related Constants;
+        // Buffers and state
         int curr_slot = 0;
         char* buffer = (char*)calloc(slots * char_per_slot, sizeof(char));
-        if (buffer == nullptr) {
-            std::cerr << "Failed to allocate buffer memory!" << std::endl;
+        if (!buffer) {
+            cerr << "Failed to allocate buffer memory!" << endl;
             return 1;
         }
+
         udp::endpoint sender_endpoint;
-        // This is the string that we use to flag when we have the complete input from the server
-        const char* end_flag = "GET:";
+        const char*    end_flag = "GET:";
+        const size_t   target_length = 7 * sizeof(char);
 
-        // Initialize Output Char* and related constants
-        const size_t target_length = 7 * sizeof(char); // This is the size of the targetID that we are looking for (Ex: SEC0001)
         char* output = (char*)malloc(80);
-        strcpy(output, "CHALLENGE_RESPONSE "); // Putting the first bit that never changes into the output buffer
+        strcpy(output, "CHALLENGE_RESPONSE ");
 
-        // Handling Each Challenge
         while (true) {
             auto recycle_start = high_resolution_clock::now();
-            // Moving output_curr Pointer to the challenge ID location
-            char* output_curr = output + 19 * sizeof(char);
+            // ... (you can keep your recycle timing code here) ...
+            auto recycle_end = high_resolution_clock::now();
+            auto dur = duration_cast<microseconds>(recycle_end - recycle_start);
+            cout << "Total time to recycle the system: " << dur.count() << " us\n";
 
-            // Copy Null Map for Storing Bids and Asks
+            // Prepare rotating slot
+            char* buff_slot = buffer + curr_slot * char_per_slot;
+            char* write     = buff_slot;
+            char* processing_ptr = buff_slot;
+            size_t buffer_length = 0;
+            size_t remaining_space = char_per_slot;
+            auto first_fragment_time = high_resolution_clock::now();
+            auto last_fragment_time  = first_fragment_time;
+
             BidAndAsk best_quotes[1001];
 
-            // Creating a pointer to the current slot in the rotating buffer
-            char* buff_slot = buffer + curr_slot * char_per_slot * sizeof(char);
-            char* write = buff_slot;
-            char* processing_ptr = buff_slot;
+            // Non-blocking receive until we see the "GET:" end_flag
+            while (buffer_length == 0 ||
+                   strncmp(buff_slot + buffer_length - 12, end_flag, 4) != 0)
+            {
+                boost::system::error_code ec;
+                size_t added = udp_socket.receive_from(
+                    boost::asio::buffer(write, remaining_space),
+                    sender_endpoint, 0, ec);
 
-            // This array of char pointers first contains a pointer to the target, a pointer to the best bid, and a pointer to the best ask
-            size_t buffer_length = 0;
-            size_t added_length = 0;
-            size_t remaining_space_in_slot = char_per_slot * sizeof(char);
-            auto recycle_end = high_resolution_clock::now();
-            auto duration3 = duration_cast<microseconds>(recycle_end - recycle_start);
-            std::cout << "Total time to recycle the system: " << duration3.count() << " us" <<std::endl;
+                if (ec == boost::asio::error::would_block) {
+                    this_thread::yield();
+                    continue;
+                }
+                else if (ec) {
+                    cerr << "Receive error: " << ec.message() << "\n";
+                    break;
+                }
 
-            auto last_fragment_time = high_resolution_clock::now();
-            auto first_fragment_time = high_resolution_clock::now();
-
-
-            // Receiving Message from the Server as Fragments
-            while (buffer_length == 0 || strncmp(buff_slot + buffer_length - 12 * sizeof(char) , end_flag, 4*sizeof(char)) != 0) {
-                added_length = udp_socket.receive_from(boost::asio::buffer(write, remaining_space_in_slot), sender_endpoint);
-                if (write == buff_slot){
+                if (write == buff_slot) {
                     first_fragment_time = high_resolution_clock::now();
                 }
                 last_fragment_time = high_resolution_clock::now();
-                buffer_length += added_length;
-                write += added_length;
-                remaining_space_in_slot -= added_length;
-                // Processes each incoming line and returns true if a complete line was processed
-                // If a complete line was processed, processing_ptr is advanced to the next line
-                // If a complete line is not processed (meaning an incomplete input / waiting for next fragment)
-                // returns false and doese not advance processing_ptr, thus waiting for the next fragment 
-                // Experiment with changing this to a "handle_fragment()" - add while loop inside of function
-                // to avoid iterative calls to the function!!!!!
-                while (handle_line(processing_ptr, write, best_quotes)){}
+
+                buffer_length     += added;
+                write             += added;
+                remaining_space   -= added;
+
+                // process any complete lines in this fragment
+                while (handle_line(processing_ptr, write, best_quotes)) {}
             }
 
-            // Finding components and saving their locations as ptrs - waiting for complete message until processing
-            char* target = buff_slot + buffer_length - target_length - sizeof(char);
+            // Extract target ID, quotes, build response
+            char* target = buff_slot + buffer_length - target_length - 1;
+            BidAndAsk q = best_quotes[char_id_to_int(target + 3)];
+            char* output_curr = output + 19;
 
-            BidAndAsk best_quote = best_quotes[char_id_to_int(target+3*sizeof(char))];
-            // Constructing Output and Moving output_curr Pointer to Next Component With Each Line
-            add_challenge_id(output_curr, buff_slot, buffer_length);
-            add_ticker(output_curr, target, target_length);
-            add_bid(output_curr, best_quote.bid);
-            add_ask(output_curr, best_quote.ask);
+            add_challenge_id       (output_curr, buff_slot, buffer_length);
+            add_ticker             (output_curr, target, target_length);
+            add_bid                (output_curr, q.bid);
+            add_ask                (output_curr, q.ask);
             add_trader_name_and_terminate(output_curr, trader_name, trader_name_length);
-            // Sending Messages to the Server
-            boost::asio::write(tcp_socket, boost::asio::buffer(output, output_curr - output));
-            auto end = high_resolution_clock::now();
-            auto duration1 = duration_cast<microseconds>(end - first_fragment_time);
-            auto duration2 = duration_cast<microseconds>(end - last_fragment_time);
 
-            std::cout << '\n' << "SENT: " << std::string(output, output_curr - output);
-            std::cout << "Time from first fragment to response: " << duration1.count() << " us" <<std::endl;
-            std::cout << "Time from last fragment to response: " << duration2.count() << " us" <<std::endl;
+            // Send via TCP
+            boost::asio::write(tcp_socket,
+                               boost::asio::buffer(output,
+                                                   output_curr - output));
 
-            // Resetting everything after completing response
+            auto end_time = high_resolution_clock::now();
+            auto d1 = duration_cast<microseconds>(end_time - first_fragment_time);
+            auto d2 = duration_cast<microseconds>(end_time - last_fragment_time);
+
+            cout << "\nSENT: "
+                 << string(output, output_curr - output) << "\n"
+                 << "Time from first fragment to response: "
+                 << d1.count() << " us\n"
+                 << "Time from last fragment to response:  "
+                 << d2.count() << " us\n";
+
             curr_slot = (curr_slot + 1) % slots;
         }
-    } catch (std::exception& e) {
-        // If Anything Goes Wrong!
-        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    catch (std::exception& e) {
+        cerr << "Error: " << e.what() << endl;
         return 1;
     }
     return 0;
 }
 
-int main() { 
-    trade(1, 40000, "SebsBoys");
+int main() {
+    trade(2, 50000, "SebsBoys");
     return 0;
 }
